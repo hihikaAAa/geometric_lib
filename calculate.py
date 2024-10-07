64b6c1e59586d0be57b5b0ef7158c789f8fa61ba
Sergei Apanasevich <heheka800@gmail.com>
2024-10-07 16:46:11 +0300

    Revert "Instructions"

--- calculate.py
+++ calculate.py
@@ -7,8 +7,6 @@ funcs = ['perimeter', 'area']
 sizes = {}
 
 def calc(fig, func, size):
-	'''Функция принимает в себя первым аргументом имя фигуры, вторым аргусентом - то значение,
-	которое функция должна получить у данной, третьим - размер, чтобы получить данное значение'''
 	assert fig in figs
 	assert func in funcs
 
@@ -21,15 +19,12 @@ if __name__ == "__main__":
 	size = list()
     
 	while fig not in figs:
-		'''Необходимо ввести фигуру, с которой будут произуведены операции'''
 		fig = input(f"Enter figure name, avaliable are {figs}:\n")
 	
 	while func not in funcs:
-		'''Необходимо ввести функцию, которая будет применена к фигуре'''
 		func = input(f"Enter function name, avaliable are {funcs}:\n")
 	
 	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		'''Необходимо ввести размер нужной фигуры'''
 		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
 	
 	calc(fig, func, size)
--- circle.py
+++ circle.py
@@ -2,11 +2,9 @@ import math
 
 
 def area(r):
-    ''' Принимает число, возвращает площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
-    '''Принимает число, возвращает периметр'''
     return 2 * math.pi * r
 
